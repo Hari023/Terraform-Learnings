78e8f14ad47c19a88b09911f311370394631d754
Hariharan <105987054+Hari023@users.noreply.github.com>
2024-11-16 02:42:29 +0530

    Update backend.tf

--- Statefile_Remotebackend/backend.tf
+++ Statefile_Remotebackend/backend.tf
@@ -1,3 +1,5 @@
+////statefile consists of all the security realated informations and details about the infrastructure that we are working By using the remote backend we can Use S3 bucket this will store the statefile information so the information is not compramised.
+
 terraform {
   backend "s3" {
     bucket         = "hari-s3-demo-xyz" # change this
